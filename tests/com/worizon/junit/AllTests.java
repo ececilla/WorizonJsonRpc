907e08a7ab1c6e894da2b26335404d4d12c35de3
ececilla <ececilla@server>
2013-09-30 03:13:10 +0200

    Added jsonrpc request test to all tests.

--- tests/com/worizon/junit/AllTests.java
+++ tests/com/worizon/junit/AllTests.java
@@ -3,10 +3,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.worizon.junit.jsonrequest.JsonRpcRequestTest;
 import com.worizon.junit.jsonresponse.JsonRpcResponseSuite;
 
 @RunWith(Suite.class)
-@SuiteClasses({ JsonRpcResponseSuite.class })
+@SuiteClasses({ JsonRpcResponseSuite.class,
+				JsonRpcRequestTest.class})
 public class AllTests {
 
 }

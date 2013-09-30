27d62c40b6a6a93f3f8d24fe54ec147117376aa1
ececilla <ececilla@server>
2013-09-30 03:11:05 +0200

    Changed params map, from hashmap to linkedhashmap.

--- src/com/worizon/jsonrpc/JsonRpcRequest.java
+++ src/com/worizon/jsonrpc/JsonRpcRequest.java
@@ -1,7 +1,7 @@
 package com.worizon.jsonrpc;
 
 import java.io.Serializable;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import com.google.gson.Gson;
@@ -25,12 +25,12 @@ public class JsonRpcRequest extends JsonRpc implements Serializable {
 	public JsonRpcRequest( String method ){
 		
 		super("2.0", System.currentTimeMillis() );
-		this.params = new HashMap<String, Object>();
+		this.params = new LinkedHashMap<String, Object>();
 		this.method = method; 
 
 	}
 	
-	public JsonRpcRequest( String method, Map<String, Object> params){
+	public JsonRpcRequest( String method, Map<String, Object> params ){
 		
 		super("2.0", System.currentTimeMillis() );		
 		this.params = params;
@@ -53,6 +53,17 @@ public class JsonRpcRequest extends JsonRpc implements Serializable {
 	public void addParam(String key, Object value){
 		
 		params.put(key, value);
+	}
+	
+	@Override
+	public boolean equals( Object obj ){
+		try{
+			
+			JsonRpcRequest req = (JsonRpcRequest)obj;
+			return req.id.longValue() == id.longValue();
+		}catch( ClassCastException cce){
+			return false;
+		}		 
 	}
 	
 	

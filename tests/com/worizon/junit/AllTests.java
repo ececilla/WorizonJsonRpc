efb840a0e3d5e408010dcd2042eec8d8a910ef37
ececilla <ececilla@server>
2013-10-01 03:43:43 +0200

    Added BooleanTypeAdapter tests. Modified AllTests.

--- tests/com/worizon/junit/AllTests.java
+++ tests/com/worizon/junit/AllTests.java
@@ -5,10 +5,12 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.worizon.junit.jsonrequest.JsonRpcRequestTest;
 import com.worizon.junit.jsonresponse.JsonRpcResponseSuite;
+import com.worizon.junit.rpc.RpcProxyTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ JsonRpcResponseSuite.class,
-				JsonRpcRequestTest.class})
+				JsonRpcRequestTest.class,
+				RpcProxyTest.class})
 public class AllTests {
 
 }
---
+++ tests/com/worizon/junit/gson/BooleanTypeAdapterTest.java
@@ -0,0 +1,78 @@
+package com.worizon.junit.gson;
+
+import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.worizon.jsonrpc.BooleanTypeAdapter;
+import com.worizon.jsonrpc.JsonRpcResponse;
+import static org.junit.Assert.*;
+
+public class BooleanTypeAdapterTest {
+	
+	class Foo{
+		boolean resultPri; 
+		Boolean resultObj;
+	}
+	
+	@Test
+	public void testParseTrue() throws Exception{
+		
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Boolean.class, new BooleanTypeAdapter());
+		builder.registerTypeAdapter(boolean.class, new BooleanTypeAdapter());
+		String message = "{\"resultPri\":true,\"resultObj\":true}";
+		Gson gson = builder.create();
+		Foo b = (Foo)gson.fromJson(message, Foo.class);
+		
+		
+		assertEquals(true, b.resultPri);
+		assertEquals(true, b.resultObj);
+	}
+	
+	@Test
+	public void testParseFalse() throws Exception{
+		
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Boolean.class, new BooleanTypeAdapter());
+		builder.registerTypeAdapter(boolean.class, new BooleanTypeAdapter());
+		String message = "{\"resultPri\":false,\"resultObj\":false}";
+		Gson gson = builder.create();
+		Foo b = (Foo)gson.fromJson(message, Foo.class);
+		
+		
+		assertEquals(false, b.resultPri);
+		assertEquals(false, b.resultObj);
+	}
+	
+	@Test
+	public void testParse1() throws Exception{
+		
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Boolean.class, new BooleanTypeAdapter());
+		builder.registerTypeAdapter(boolean.class, new BooleanTypeAdapter());
+		String message = "{\"resultPri\":1,\"resultObj\":1}";
+		Gson gson = builder.create();
+		Foo b = (Foo)gson.fromJson(message, Foo.class);
+		
+		
+		assertEquals(true, b.resultPri);
+		assertEquals(true, b.resultObj);
+	}
+	
+	@Test
+	public void testParse0() throws Exception{
+		
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Boolean.class, new BooleanTypeAdapter());
+		builder.registerTypeAdapter(boolean.class, new BooleanTypeAdapter());
+		String message = "{\"resultPri\":0,\"resultObj\":0}";
+		Gson gson = builder.create();
+		Foo b = (Foo)gson.fromJson(message, Foo.class);
+		
+		
+		assertEquals(false, b.resultPri);
+		assertEquals(false, b.resultObj);
+	}
+
+}

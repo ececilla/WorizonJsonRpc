9f51c536fd1e10c6ee43040bd064439d273dc107
ececilla <ececilla@server>
2013-11-04 23:53:50 +0100

    more tests added.

--- tests/com/worizon/junit/jsonresponse/JsonRpcErrorResponseTest.java
+++ tests/com/worizon/junit/jsonresponse/JsonRpcErrorResponseTest.java
@@ -4,20 +4,57 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.worizon.jsonrpc.JsonRpcException;
 import com.worizon.jsonrpc.JsonRpcResponse;
 
 public class JsonRpcErrorResponseTest {
 	
 
 	@Test
-	public void testError() throws Exception{
+	public void testErrorWithCodeWithMessageWithData() throws Exception{
+		
+		String message = "{\"jsonrpc\": \"2.0\", \"error\": {\"code\":-100, \"message\":\"test error\",\"data\":\"foo\"}, \"id\": 2}";		
+		JsonRpcResponse<Integer> res = new JsonRpcResponse<Integer>(message, Integer.class);
+		assertEquals( -100, res.getError().getCode() );
+		assertEquals("test error", res.getError().getMessage());	
+		assertEquals("foo", (String)res.getError().getData());
+		assertNull(res.getResult());		
+	}
+	
+	@Test
+	public void testErrorWithCodeWithMessage() throws Exception{
 		
 		String message = "{\"jsonrpc\": \"2.0\", \"error\": {\"code\":-100, \"message\":\"test error\"}, \"id\": 2}";		
 		JsonRpcResponse<Integer> res = new JsonRpcResponse<Integer>(message, Integer.class);
 		assertEquals( -100, res.getError().getCode() );
 		assertEquals("test error", res.getError().getMessage());		
-		assertNull(res.getResult());
+		assertNull(res.getResult());		
+	}
 		
-	}	
+	@Test
+	public void testErrorWithCodeMessageNull() throws Exception{
+		
+		String message = "{\"jsonrpc\": \"2.0\", \"error\": {\"code\":-100, \"message\":null}, \"id\": 2}";		
+		JsonRpcResponse<Integer> res = new JsonRpcResponse<Integer>(message, Integer.class);
+		assertEquals( -100, res.getError().getCode() );
+		assertNull(res.getError().getMessage());				
+		assertNull(res.getResult());		
+	}
+	
+	@Test(expected=JsonRpcException.class)
+	public void testErrorWithCodeMessageNotFound() throws Exception{
+		
+		String message = "{\"jsonrpc\": \"2.0\", \"error\": {\"code\":-100}, \"id\": 2}";		
+		new JsonRpcResponse<Integer>(message, Integer.class);
+				
+	}
+	
+	@Test(expected=JsonRpcException.class)
+	public void testErrorCodeNotFound() throws Exception{
+		
+		String message = "{\"jsonrpc\": \"2.0\", \"error\": {\"message\":null}, \"id\": 2}";		
+		new JsonRpcResponse<Void>(message, Void.class);
+		
+	}
 	
 }
--- tests/com/worizon/junit/jsonresponse/JsonRpcMixedTypesArrayResponseTest.java
+++ tests/com/worizon/junit/jsonresponse/JsonRpcMixedTypesArrayResponseTest.java
@@ -10,10 +10,34 @@ import com.worizon.jsonrpc.JsonRpcResponse;
 public class JsonRpcMixedTypesArrayResponseTest {
 	
 
-	@Test(expected = JsonSyntaxException.class)
-	public void testResult() throws Exception{
+	@Test
+	public void testResult1() throws Exception{
 		
 		String message = "{\"jsonrpc\": \"2.0\", \"result\": [-19,10.5,67], \"id\": 2}";		
+		JsonRpcResponse<int[]> res = new JsonRpcResponse<int[]>(message, int[].class);			
+		assertArrayEquals(new int[]{-19,10,67}, res.getResult());
+	}
+	
+	@Test(expected=JsonSyntaxException.class)
+	public void testResult2() throws Exception{
+		
+		String message = "{\"jsonrpc\": \"2.0\", \"result\": [-19,10.5,67,\"fooo\"], \"id\": 2}";		
+		JsonRpcResponse<int[]> res = new JsonRpcResponse<int[]>(message, int[].class);			
+		
+	}
+	
+	@Test(expected=JsonSyntaxException.class)
+	public void testResult3() throws Exception{
+		
+		String message = "{\"jsonrpc\": \"2.0\", \"result\": [-19,10.5,67,[]], \"id\": 2}";		
+		JsonRpcResponse<int[]> res = new JsonRpcResponse<int[]>(message, int[].class);			
+		
+	}
+	
+	@Test(expected=JsonSyntaxException.class)
+	public void testResult4() throws Exception{
+		
+		String message = "{\"jsonrpc\": \"2.0\", \"result\": [-19,10.5,67,{}], \"id\": 2}";		
 		JsonRpcResponse<int[]> res = new JsonRpcResponse<int[]>(message, int[].class);			
 		
 	}

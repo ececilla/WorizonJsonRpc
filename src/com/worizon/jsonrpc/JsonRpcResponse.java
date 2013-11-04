a748d9a02868adb8dbdd134b0a4b6a487ffc604e
ececilla <ececilla@server>
2013-11-04 23:53:23 +0100

    refactored constructor de provide more checks and validations on response string.

--- src/com/worizon/jsonrpc/JsonRpcResponse.java
+++ src/com/worizon/jsonrpc/JsonRpcResponse.java
@@ -27,26 +27,42 @@ public class JsonRpcResponse<T> extends JsonRpc {
 	private T result = null;
 	private JsonRpcError error = null;	
 	
-	public JsonRpcResponse( String json_str, Class<? extends T> clazz ) throws InstantiationException, IllegalAccessException {
+	public JsonRpcResponse( String json_str, Class<? extends T> clazz ) throws InstantiationException, IllegalAccessException, JsonRpcException {
 				
 		JsonParser parser = new JsonParser();
 		JsonObject root = parser.parse(json_str).getAsJsonObject();				
-		jsonrpc = root.get("jsonrpc").getAsString();		
+		jsonrpc = root.get("jsonrpc").getAsString();
+		if(!jsonrpc.equals("2.0"))
+			throw new JsonRpcException("Version not supported:" + jsonrpc);
 
-		if( !root.get("id").isJsonNull() )
+		if( root.has("id") && !root.get("id").isJsonNull() )
 			id = root.get("id").getAsLong();
+		else
+			throw new JsonRpcException("Id not found or null");
 		
-		Gson gson = getDecodingHelper();
+		Gson gson = getDeserializeHelper();
 		if( root.has("result") ){
 			if( !root.get("result").isJsonNull() ){ 
-				String result_str = gson.toJson(root.get("result"));					
-				result = gson.fromJson(result_str, clazz);
+				
+				result = gson.fromJson(root.get("result"), clazz);
 			}else
 				result = null;			
+		}else if( root.has("error") ){
+			if(!root.get("error").isJsonNull()){
+				
+				JsonObject jsonError = root.get("error").getAsJsonObject();
+				if( jsonError.has("code") && jsonError.has("message")){
+					
+					String error_str = gson.toJson(root.get("error"));
+					error = gson.fromJson(error_str, JsonRpcError.class);
+				}else{
+					throw new JsonRpcException("Error code or message not found");
+				}
+			}else{
+				throw new JsonRpcException("Error field is null");
+			}
 		}else{
-			
-			String error_str = gson.toJson(root.get("error"));
-			error = gson.fromJson(error_str, JsonRpcError.class);
+			throw new JsonRpcException("Neither result nor error fields present");
 		}
 	}			
 	

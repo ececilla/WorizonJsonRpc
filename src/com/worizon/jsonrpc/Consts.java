33b593cb7dea5d000f78582d22375ce930b3fe3d
ececilla <ececilla@server>
2013-11-06 00:55:18 +0100

    Consts class with error codes by now.

---
+++ src/com/worizon/jsonrpc/Consts.java
@@ -0,0 +1,14 @@
+package com.worizon.jsonrpc;
+
+public final class Consts {
+	
+	private Consts(){}
+	
+	public static final int INVALID_REQUEST_CODE = -32600;
+	public static final int METHOD_NOT_FOUND_CODE = -32601;
+	public static final int INVALID_PARAMS_CODE = -32602;
+	public static final int INTERNAL_ERROR_CODE = -32603;
+	public static final int PARSE_ERROR_CODE = -32700;
+	
+
+}

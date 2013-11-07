7e6b7eed45b946ba6e6f0b9dc0e6e054e2012fdc
ececilla <ececilla@gmail.com>
2013-11-07 15:41:08 +0100

    refactored comments and some internal variables.

--- src/com/worizon/jsonrpc/JsonRpc.java
+++ src/com/worizon/jsonrpc/JsonRpc.java
@@ -8,7 +8,8 @@ import com.worizon.jsonrpc.gson.NonExposeExclusionEstrategy;
 
 /**
  * 
- * Base class for JsonRpc request and response objects. 
+ * Base class for JsonRpc request and response objects. Common fields for both request and response 
+ * objects are defined here as well as static helpers methods.
  * 
  * @author Enric Cecilla
  *
@@ -17,15 +18,15 @@ public abstract class JsonRpc {
 	
 	private static BooleanTypeAdapter booleanTypeAdapter = new BooleanTypeAdapter(); 
 	private static ExclusionStrategy nonExposeStrategy = new NonExposeExclusionEstrategy();	
-	private static GsonBuilder serializeBuilder = new GsonBuilder();
-	private static GsonBuilder deserializeBuilder = new GsonBuilder();
+	private static GsonBuilder serializeGsonBuilder = new GsonBuilder();
+	private static GsonBuilder deserializeGsonBuilder = new GsonBuilder();
 	static{
 		
-		serializeBuilder.setExclusionStrategies(nonExposeStrategy);
-		serializeBuilder.registerTypeAdapter(Boolean.class, booleanTypeAdapter);
-		serializeBuilder.registerTypeAdapter(boolean.class, booleanTypeAdapter);		
-		deserializeBuilder.registerTypeAdapter(Boolean.class, booleanTypeAdapter);
-		deserializeBuilder.registerTypeAdapter(boolean.class, booleanTypeAdapter);
+		serializeGsonBuilder.setExclusionStrategies(nonExposeStrategy);
+		serializeGsonBuilder.registerTypeAdapter(Boolean.class, booleanTypeAdapter);
+		serializeGsonBuilder.registerTypeAdapter(boolean.class, booleanTypeAdapter);		
+		deserializeGsonBuilder.registerTypeAdapter(Boolean.class, booleanTypeAdapter);
+		deserializeGsonBuilder.registerTypeAdapter(boolean.class, booleanTypeAdapter);
 	}
 	
 	protected String jsonrpc = null;	
@@ -41,31 +42,52 @@ public abstract class JsonRpc {
 		this.id = id;
 	}		
 	
+	/**
+	 * Gets Gson main facade configured to serialize pojos into a json string
+	 */
 	protected static Gson getSerializeHelper(){
 				
-		return serializeBuilder.create();
+		return serializeGsonBuilder.create();
 	}
 	
+	/**
+	 * Gets Gson main facade configured to deserialize a json string into a pojo.
+	 */
 	protected static Gson getDeserializeHelper(){
 				
-		return deserializeBuilder.create();
+		return deserializeGsonBuilder.create();
 	}
-		
+	
+	/**
+	 * Gets the version of this request or response.
+	 * @return the jsonrpc version. Should be "2.0"
+	 */
 	public String getVersion() {
 		
 		return jsonrpc;
 	}
 
+	/**
+	 * Sets the version of the request or reponse.
+	 * @param jsonrpc: 
+	 */
 	public void setVersion( String jsonrpc ) {
 		
 		this.jsonrpc = jsonrpc;
 	}
-
+	
+	/**
+	 * Gets the id of the request or response.
+	 * @return The id as a timestamp. 
+	 */
 	public Long getId() {
 		
 		return id;
 	}
 
+	/**
+	 * Sets the if of the request, response.
+	 */
 	public void setId( Long id ) {
 		
 		this.id = id;

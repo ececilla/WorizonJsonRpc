e40d326de0d158b1dc01da8b60023fef57c9383e
ececilla <ececilla@server>
2013-11-20 01:47:36 +0100

    tests for type handling.

---
+++ tests/com/worizon/junit/rpc/TypesUtilTest.java
@@ -0,0 +1,58 @@
+package com.worizon.junit.rpc;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.worizon.jsonrpc.TypesUtil;
+
+public class TypesUtilTest {
+	
+	@Test
+	public void testAllTrue(){
+		
+		Object objs[] = new Object[]{new Integer(1), new Integer(2), new Integer(3), new Integer(4)};
+		assertTrue( TypesUtil.all(objs, Integer.class) );
+		
+	}
+	
+	@Test
+	public void testAllFalse(){
+		
+		Object objs[] = new Object[]{new Integer(1), new Double(2), new Integer(3), new Integer(4)};
+		assertFalse( TypesUtil.all(objs, Integer.class) );
+		
+	}
+	
+	@Test
+	public void testAnyTrue(){
+		
+		Object objs[] = new Object[]{new Double(1), new Long(2), new Integer(3), new Float(4)};
+		assertTrue( TypesUtil.any(objs, Integer.class) );
+		
+	}
+	
+	@Test
+	public void testAnyFalse(){
+		
+		Object objs[] = new Object[]{new Double(1), new Long(2), new String(""), new Float(4)};
+		assertFalse( TypesUtil.any(objs, Integer.class) );
+		
+	}
+	
+	@Test
+	public void testNoneTrue(){
+		
+		Object objs[] = new Object[]{new Double(1), new Long(2), new Short((short)0), new Float(4)};
+		assertTrue( TypesUtil.none(objs, Integer.class) );
+		
+	}
+	
+	@Test
+	public void testNoneFalse(){
+		
+		Object objs[] = new Object[]{new Double(1), new Integer(2), new Short((short)0), new Float(4)};
+		assertFalse( TypesUtil.none(objs, Integer.class) );
+		
+	}
+	
+	
+}

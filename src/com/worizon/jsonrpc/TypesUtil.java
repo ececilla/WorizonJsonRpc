e52fa25cdf571f1b2de0e78d636ad7a14b5c41ed
ececilla <ececilla@server>
2013-11-22 01:38:46 +0100

    Refactored class.

--- src/com/worizon/jsonrpc/TypesUtil.java
+++ src/com/worizon/jsonrpc/TypesUtil.java
@@ -1,46 +1,81 @@
 package com.worizon.jsonrpc;
 
 /**
- * 
+ * Utility class to check array object types.
  * @author Enric Cecilla
  * @since 1.0.0
  */
-public class TypesUtil {
-	
-	/**
-	 * 
-	 */
-	public static boolean all( Object[] objs, Class<?> clazz ){
+public final class TypesUtil {
 		
-		for( Object obj : objs ){
-			if( !clazz.isInstance(obj) )
-				return false;
-		}
-		return true;
+	private TypesUtil(){}
+	
+	public interface TypeChecker{
+		
+		public boolean haveType(Class<?> clazz);
 	}
 	
 	/**
-	 * 
+	 * Checks whether all objects in this array are of the specified type or not.
+	 * @param objs The array of objects to check.
+	 * @param clazz The type to check against.
+	 * @return True if all objects are from this Class, false otherwise. 
 	 */
-	public static boolean any( Object[] objs, Class<?> clazz ){
+	public static TypeChecker all( final Object [] objs ){
 		
-		for( Object obj : objs ){			
-			if( clazz.isInstance(obj) )
-				return true;
-		}
-		return false;
+		return new TypeChecker(){
+			
+			public boolean haveType(Class<?> clazz){
+				
+				for( Object obj : objs ){
+					if( !clazz.isInstance(obj) )
+						return false;
+				}
+				return true;				
+			}
+		};
 	}
 	
 	/**
-	 * 
+	 * Checks whether at least one object in this array is of the specified type or not.
+	 * @param objs The array of objects to check.
+	 * @param clazz The type to check against.
+	 * @return True if al least one object is an instance of this class, false otherwise.
 	 */
-	public static boolean none( Object[] objs, Class<?> clazz ){
+	public static TypeChecker any( final Object [] objs ){
 		
-		for( Object obj : objs ){
-			if( clazz.isInstance(obj) )
-				return false;
-		}
-		return true;
+		return new TypeChecker(){
+			
+			public boolean haveType(Class<?> clazz){
+				
+				for( Object obj : objs ){			
+					if( clazz.isInstance(obj) )
+						return true;
+				}
+				return false;				
+			}
+		};
 	}
+	
+	/**
+	 * Checks whether none object in this array is of the specified type or not.
+	 * @param objs The array of objects to check.
+	 * @param clazz The type to check against.
+	 * @return True if none elements are an instance of this class, false otherwise.
+	 */
+	public static TypeChecker none( final Object [] objs ){
+		
+		return new TypeChecker(){
+			
+			public boolean haveType(Class<?> clazz){
+				
+				for( Object obj : objs ){
+					if( clazz.isInstance(obj) )
+						return false;
+				}
+				return true;				
+			}
+		};
+	}
+	
 
 }

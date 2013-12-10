1ba1bac8adf6f00fce0e2f83a8c6f28223e845f0
ececilla <ececilla@server>
2013-12-11 00:28:32 +0100

    Cleaned imports section.

--- src/com/worizon/jsonrpc/JsonRpcResponse.java
+++ src/com/worizon/jsonrpc/JsonRpcResponse.java
@@ -2,10 +2,8 @@ package com.worizon.jsonrpc;
 
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 
 

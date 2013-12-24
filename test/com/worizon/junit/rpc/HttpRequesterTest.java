87764b7b3eeadc47e8b21d4c23d2d5d3f6ac418e
ececilla <ececilla@server>
2013-12-24 02:49:15 +0100

    added tests for synchronized methods.

--- test/com/worizon/junit/rpc/HttpRequesterTest.java
+++ test/com/worizon/junit/rpc/HttpRequesterTest.java
@@ -3,10 +3,12 @@ package com.worizon.junit.rpc;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import java.io.IOException;
 import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 
+import org.hamcrest.core.IsNot;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,15 +20,16 @@ import com.worizon.test.TestServer;
 public class HttpRequesterTest {
 	
 	private TestServer server;
-	private TestServer.IRequester http;
+	private TestServer.IRequester http;	
+	private HttpRequester myRequester;
 	
 	@Before
 	public void setUp() throws Exception{
-				
+		
+		myRequester = new HttpRequester();
 		server = new TestServer(4444);
 		server.setAdapter(new TestServer.IRequester() {
-			
-			private HttpRequester myRequester = new HttpRequester();
+									
 			@Override
 			public void setEndpoint(String endpoint) throws MalformedURLException{
 				
@@ -119,6 +122,7 @@ public class HttpRequesterTest {
 		assertThat(req, is(notNullValue()));
 		assertThat(req.getMethod(), is("test") );
 		assertThat(req.getVersion(), is("2.0") );
+		assertThat(req.getParams(), is(notNullValue()));
 		assertThat(req.getId(), is(1000L));				
 	}
 	
@@ -147,8 +151,111 @@ public class HttpRequesterTest {
 		requester.setReadTimeout(1000);
 		requester.request("test");			
 	}
-		
 	
+	@Test
+	public void testSetEndpointSynchronized() throws Exception{
+		
+		server.setIdleTime(2000);							
+		Thread t = new Thread( new Runnable() {
+			
+			@Override
+			public void run(){
+				
+				try{
+					Thread.sleep(500);//secondary thread waits for main thread to get lock on requester object
+					http.setEndpoint("http://foobar.com");
+					assertThat(server.getBody(), is("bar"));	
+				}catch(Exception ex){ ex.printStackTrace();}
+			}
+		});
+		t.start();
+		http.doRequest("bar");//main thread gets lock on requester object
+									
+	}
+		
+	@Test
+	public void testSetRequestRetriesSynchronized() throws Exception{
+		
+		server.setIdleTime(2000);							
+		Thread t = new Thread( new Runnable() {
+			
+			@Override
+			public void run(){
+				
+				try{
+					Thread.sleep(500);//secondary thread waits for main thread to get lock on requester object
+					myRequester.setRequestRetries(0);
+					assertThat(server.getBody(), is("bar"));	
+				}catch(Exception ex){ ex.printStackTrace();}
+			}
+		});
+		t.start();
+		http.doRequest("bar");//main thread gets lock on requester object
+									
+	}
+	
+	@Test
+	public void testSetReadTimeoutSynchronized() throws Exception{
+		
+		server.setIdleTime(2000);							
+		Thread t = new Thread( new Runnable() {
+			
+			@Override
+			public void run(){
+				
+				try{
+					Thread.sleep(500);//secondary thread waits for main thread to get lock on requester object
+					myRequester.setReadTimeout(0);
+					assertThat(server.getBody(), is("bar"));	
+				}catch(Exception ex){ ex.printStackTrace();}
+			}
+		});
+		t.start();
+		http.doRequest("bar");//main thread gets lock on requester object
+									
+	}
+	
+	@Test
+	public void testSetConnectTimeoutSynchronized() throws Exception{
+		
+		server.setIdleTime(2000);							
+		Thread t = new Thread( new Runnable() {
+			
+			@Override
+			public void run(){
+				
+				try{
+					Thread.sleep(500);//secondary thread waits for main thread to get lock on requester object
+					myRequester.setConnectTimeout(0);
+					assertThat(server.getBody(), is("bar"));	
+				}catch(Exception ex){ ex.printStackTrace();}
+			}
+		});
+		t.start();
+		http.doRequest("bar");//main thread gets lock on requester object
+									
+	}
+	
+	@Test
+	public void testAddTransformersSynchronized() throws Exception{
+		
+		server.setIdleTime(2000);							
+		Thread t = new Thread( new Runnable() {
+			
+			@Override
+			public void run(){
+				
+				try{
+					Thread.sleep(500);//secondary thread waits for main thread to get lock on requester object
+					myRequester.addTransformers(null);
+					assertThat(server.getBody(), is("bar"));	
+				}catch(Exception ex){ ex.printStackTrace();}
+			}
+		});
+		t.start();
+		http.doRequest("bar");//main thread gets lock on requester object
+									
+	}
 	
 
 }
